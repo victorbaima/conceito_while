3ce58a57e5b42ac2fbf6a3911c1408b5bc8081d9
victorbaima <victor.baima@hotmail.com>
2020-06-18 07:08:55 -0300

    Conceito While 1.0

---
+++ Conceito_While/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Conceito_While
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.btnTeste = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(161, 42);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(35, 13);
+            this.lblNumero.TabIndex = 0;
+            this.lblNumero.Text = "label1";
+            // 
+            // btnTeste
+            // 
+            this.btnTeste.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTeste.Location = new System.Drawing.Point(106, 104);
+            this.btnTeste.Name = "btnTeste";
+            this.btnTeste.Size = new System.Drawing.Size(148, 60);
+            this.btnTeste.TabIndex = 1;
+            this.btnTeste.Text = "Teste While";
+            this.btnTeste.UseVisualStyleBackColor = true;
+            this.btnTeste.Click += new System.EventHandler(this.btnTeste_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 258);
+            this.Controls.Add(this.btnTeste);
+            this.Controls.Add(this.lblNumero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Button btnTeste;
+    }
+}
+
---
+++ Conceito_While/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conceito_While
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTeste_Click(object sender, EventArgs e)
+        {
+            int c = 0;
+            while (c <10000)
+            {
+                c = c + 1;
+                lblNumero.Text = Convert.ToString(c);
+                lblNumero.Refresh();
+
+                /*Declaração da variável c, inicializada com zero.
+                 * Condição: enquanto o c for menor que 10000 será executado o laço
+                 * incremento.
+                 * Refresh é utilizado para que haja a variação dos números*/
+
+                 
+            }
+        }
+    }
+}
